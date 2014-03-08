eb2efc06c2c3709f4535d62e85917ac2c6277b51
axzxc1236 <axzxc1236@gmail.com>
2014-03-08 22:57:35 +0800

    version

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -157,7 +157,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "mod-kit v0.1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
