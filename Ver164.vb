bc24936fb5a15fd728dd293c33f5915bb83ab047
axzxc1236 <axzxc1236@gmail.com>
2014-03-23 09:02:50 +0800

    fix "count" in 1.6.4

--- Ver164.vb
+++ Ver164.vb
@@ -462,6 +462,14 @@
         If CheckBox7.Enabled = True And CheckBox7.Checked = True Then count += 1
         If CheckBox8.Enabled = True And CheckBox8.Checked = True Then count += 1
         If CheckBox9.Enabled = True And CheckBox9.Checked = True Then count += 1
+        If CheckBox10.Enabled = True And CheckBox10.Checked = True Then count += 1
+        If CheckBox11.Enabled = True And CheckBox11.Checked = True Then count += 1
+        If CheckBox12.Enabled = True And CheckBox12.Checked = True Then count += 1
+        If CheckBox13.Enabled = True And CheckBox13.Checked = True Then count += 1
+        If CheckBox14.Enabled = True And CheckBox14.Checked = True Then count += 1
+        If CheckBox15.Enabled = True And CheckBox15.Checked = True Then count += 1
+        If CheckBox16.Enabled = True And CheckBox16.Checked = True Then count += 1
+        If CheckBox16.Enabled = True And CheckBox16.Checked = True Then count += 1
         If Not My.Computer.FileSystem.FileExists(Main.TextBox1.Text & "\libraries\org\scala-lang\scala-compiler\2.10.2\scala-compiler-2.10.2.jar") Then count += 1
         If Not My.Computer.FileSystem.FileExists(Main.TextBox1.Text & "\libraries\org\scala-lang\scala-library\2.10.2\scala-library-2.10.2.jar") Then count += 1
 
